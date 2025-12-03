0c7ebde5a7b46dce202dc1d67a53271b8de95abc
InasLew <26838345+InaSLew@users.noreply.github.com>
2025-12-03 06:33:55 +0100

    currently stuck in day2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,20 +1,43 @@
-# Goal: identifying any number in a given range that when stringfied and split in half, its first and second half are identical.
-# Cible : Identifier tous les numéros dans les séries numériques dont, quand découpés en moitié, la première partie est identique à la deuxième.
-# Piège -> le paramètre de stop dans la fonction range(start, stop, step)
-
-file = open("day2input.txt", "r")
+file = open("test.txt", "r")
 inputs = file.read().split(",")
 
 invalidIDSum = 0
 
-for input in inputs:
-    firstLastIDList = input.split("-")
-    firstID = int(firstLastIDList[0])
-    lastID = int(firstLastIDList[1])
-    for productID in range(firstID, lastID + 1):
-        productIDStr = str(productID)
-        productIDLen = len(productIDStr)
-        if (productIDLen % 2 == 0 and productIDStr[0: (1 if productIDLen // 2 == 1 else productIDLen // 2)] == productIDStr[productIDLen // 2:]):
-            invalidIDSum += productID
+# part 1
+# Goal: identifying any number in a given range that when stringfied and split in half, its first and second half are identical.
+# Cible : Identifier tous les numéros dans les séries numériques dont, quand découpés en moitié, la première partie est identique à la deuxième.
+# Piège -> le paramètre de stop dans la fonction range(start, stop, step)
+# for input in inputs:
+#     firstLastIDList = input.split("-")
+#     firstID = int(firstLastIDList[0])
+#     lastID = int(firstLastIDList[1])
+#     for productID in range(firstID, lastID + 1):
+#         productIDStr = str(productID)
+#         productIDLen = len(productIDStr)
+#         if (productIDLen % 2 == 0 and productIDStr[0: (1 if productIDLen // 2 == 1 else productIDLen // 2)] == productIDStr[productIDLen // 2:]):
+#             invalidIDSum += productID
 
-print(invalidIDSum)
+# part 2
+# 4174379265
+# 1303733892002 - too high
+# problems: 565656 and 1001
+lookup = dict()
+for input in inputs:
+        firstLastIDList = input.split("-")
+        firstID = int(firstLastIDList[0])
+        lastID = int(firstLastIDList[1])
+        for productID in range(firstID, lastID + 1):
+            productIDStr = str(productID)
+            productIDLen = len(productIDStr)
+            if (productIDLen % 2 == 0 and productIDStr[0: (1 if productIDLen // 2 == 1 else productIDLen // 2)] == productIDStr[productIDLen // 2:]):
+                print(productID)
+                invalidIDSum += productID
+            else:
+                for digit in productIDStr:
+                    lookup[digit] = 1 if (digit not in lookup) else lookup[digit] + 1
+                if (len(set(lookup.values())) == 1 and set(lookup.values()).pop() > 1) :
+                    print(productID)
+                    invalidIDSum += productID
+                lookup.clear()
+
+print("result: ", invalidIDSum)
